cf302409f0527ae3c85f771cbf13199cb5e71783
npronin <noxywille@yandex.ru>
2025-03-01 22:19:56 +0300

    added comments

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -21,30 +21,34 @@ public class PracticeFormTest {
     @Test
     void practiceFormTst(){
         open("/automation-practice-form");
+        // Проверка заголовка страницы
         $(".text-center").shouldHave(text("Practice Form"));
+        // Ввод фамилию, имя, пол, почту и номер телефона
         $("#firstName").setValue("Nikita");
         $("#lastName").setValue("Pronin");
         $("#userEmail").setValue("test@test.ru");
-        $("#firstName").setValue("Nikita");
         $("[for='gender-radio-1']").click();
         $("#userNumber").setValue("0123456789");
-        // Выбираем дату рождения
+        // Выбор даты рождения
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOption("1993");
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__day--019").click();
+        // Выбор предмета
         $("#subjectsInput").setValue("Science").pressEnter();
+        // Выбор хобби
         $("[for=hobbies-checkbox-2]").click();
+        // Загрузка изображения
         $("#uploadPicture").uploadFile(new File("src/test/resources/cat.jpeg"));
+        // Ввод текущего адреса
         $("#currentAddress").setValue("33321 Gr.Drive");
+        // Выбор штата
         $("#state").click();
+        // Выбор города
+        // Подтверждаем форму
+        // Проверка введеных полей
 
         sleep(5000);
 
-
-
-
-
-        sleep(2000);
     }
 }
