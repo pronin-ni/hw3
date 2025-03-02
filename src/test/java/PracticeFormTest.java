7d39a22db4425844345095d5b67579840b44c4e8
npronin <noxywille@yandex.ru>
2025-03-02 16:20:35 +0300

    added variables

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -13,6 +13,18 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTest {
+    private String firstName = "test";
+    private String lastName = "user";
+    private String email = "test@test.ru";
+    private String number = "0123456789";
+    private String birthYear = "1993";
+    private String birthMonth = "July";
+    private String subjects = "Science";
+    private String currentAddress = "33321 Gr.Drive";
+    private String fileToUpload = "cat.jpeg";
+    private String state = "Haryana";
+    private String city = "Karnal";
+
     @BeforeAll
     static void beforeAll(){
         Configuration.baseUrl = "https://demoqa.com";
@@ -24,31 +36,36 @@ public class PracticeFormTest {
         // Проверка заголовка страницы
         $(".text-center").shouldHave(text("Practice Form"));
         // Ввод фамилию, имя, пол, почту и номер телефона
-        $("#firstName").setValue("Nikita");
-        $("#lastName").setValue("Pronin");
-        $("#userEmail").setValue("test@test.ru");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
         $("[for='gender-radio-1']").click();
-        $("#userNumber").setValue("0123456789");
+        $("#userNumber").setValue(number);
         // Выбор даты рождения
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").selectOption("1993");
-        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption(birthYear);
+        $(".react-datepicker__month-select").selectOption(birthMonth);
         $(".react-datepicker__day--019").click();
-        // Выбор предмета
-        $("#subjectsInput").setValue("Science").pressEnter();
-        // Выбор хобби
+        // Выбор предмета и хобби
+        $("#subjectsInput").setValue(subjects).pressEnter();
         $("[for=hobbies-checkbox-2]").click();
         // Загрузка изображения
-        $("#uploadPicture").uploadFile(new File("src/test/resources/cat.jpeg"));
+        $("#uploadPicture").uploadFile(new File("src/test/resources/" + fileToUpload));
         // Ввод текущего адреса
-        $("#currentAddress").setValue("33321 Gr.Drive");
-        // Выбор штата
-        $("#state").click();
-        // Выбор города
+        $("#currentAddress").setValue(currentAddress);
+        // Выбор штата и города
+        $("#react-select-3-input").setValue(state).pressEnter();
+        $("#react-select-4-input").setValue(city).pressEnter();
         // Подтверждаем форму
+        $("#submit").click();
         // Проверка введеных полей
-
-        sleep(5000);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-body").shouldHave(text(firstName),
+                text(lastName),
+                text(email),
+                text(number),
+                text(fileToUpload),
+                text(currentAddress));
 
     }
 }
